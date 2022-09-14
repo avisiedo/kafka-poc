fe19a0d9ded2acfc6efe3f335ed2d6fae7a22c6d
Alejandro Visiedo <avisiedo@redhat.com>
2022-09-14 09:11:06 +0200

    poc:initial produce and consume messages, serializing them in json format
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- cmd/kafka/main.go
+++ cmd/kafka/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
@@ -15,10 +17,19 @@ import (
 	kafka "github.com/segmentio/kafka-go"
 )
 
+type MessageUpdateStatus struct {
+	RepositoryURL   string `json:"url"`
+	RepositoryCount int    `json:"count"`
+	Status          string `json:"status"`
+	Comment         string `json:"comment"`
+}
+
+type MessageKey struct {
+	Id int64 `json:"id"`
+}
 type Widget struct {
-	Name   string `form:"name"`
-	Flavor string `form:"flavor"`
-	Id     int64  `form:"id"`
+	Key     MessageKey
+	Payload MessageUpdateStatus
 }
 
 func getUrl() (string, error) {
@@ -43,18 +54,60 @@ func getTopic() (string, error) {
 
 }
 
+func getKafkaReader() (*kafka.Reader, error) {
+	var (
+		err      error
+		kafkaUrl string
+		topic    string
+	)
+	if clowder.IsClowderEnabled() {
+		if kafkaUrl, err = getUrl(); err != nil {
+			return nil, err
+		}
+		log.Printf("kafkaUrl = '%s'", kafkaUrl)
+		if topic, err = getTopic(); err != nil {
+			return nil, err
+		}
+		log.Printf("topic = '%s'", topic)
+	} else {
+		kafkaUrl = "localhost:9092"
+		topic = "repos.created"
+	}
+
+	config := kafka.ReaderConfig{
+		Topic:    topic,
+		Brokers:  []string{kafkaUrl},
+		MaxWait:  500 * time.Millisecond,
+		MinBytes: 1,
+		MaxBytes: 100,
+	}
+
+	return kafka.NewReader(config), nil
+}
+
 func getKafkaWriter() (*kafka.Writer, error) {
 	if !clowder.IsClowderEnabled() {
-		return nil, errors.New("Clowder disabled")
+		log.Println("clowder disabled")
+		kafkaUrl := "localhost:9092"
+		topic := "repos.created"
+		log.Printf("kafkaUrl = '%s'", kafkaUrl)
+		log.Printf("topic = '%s'", topic)
+		return &kafka.Writer{
+			Addr:     kafka.TCP(kafkaUrl),
+			Topic:    topic,
+			Balancer: &kafka.LeastBytes{},
+		}, nil
 	}
 	kafkaUrl, urlError := getUrl()
 	if urlError != nil {
 		return nil, urlError
 	}
+	log.Printf("kafkaUrl = '%s'", kafkaUrl)
 	topic, topicError := getTopic()
 	if topicError != nil {
 		return nil, topicError
 	}
+	log.Printf("topic = '%s'", topic)
 	return &kafka.Writer{
 		Addr:     kafka.TCP(kafkaUrl),
 		Topic:    topic,
@@ -62,36 +115,58 @@ func getKafkaWriter() (*kafka.Writer, error) {
 	}, nil
 }
 
-func sendMessage(context context.Context, writer *kafka.Writer, id int64, name string) {
-	if clowder.IsClowderEnabled() {
-		msg := kafka.Message{
-			Key:   []byte(fmt.Sprintf("id-%s", id)),
-			Value: []byte(name),
-		}
-		err := writer.WriteMessages(context, msg)
-
-		if err == nil {
-			log.Println("Sent message")
-		} else {
-			log.Fatalln(err)
-		}
-	} else {
-		log.Println("clowder disabled")
+func sendMessage(context context.Context, writer *kafka.Writer, widget *Widget) error {
+	var (
+		err     error
+		header  []byte
+		payload []byte
+	)
+	// if clowder.IsClowderEnabled() {
+	// msg := kafka.Message{
+	// 	Key:   []byte(fmt.Sprintf("id-%d", id)),
+	// 	Value: []byte(name),
+	// }
+	if widget == nil {
+		return fmt.Errorf("widget is nil")
 	}
+	if header, err = json.Marshal(widget.Key); err != nil {
+		return err
+	}
+	if payload, err = json.Marshal(widget.Payload); err != nil {
+		return err
+	}
+	msg := kafka.Message{
+		Key:   header,
+		Value: payload,
+	}
+	if err = writer.WriteMessages(context, msg); err != nil {
+		return err
+	}
+
+	return nil
+
+	// } else {
+	// 	log.Println("clowder disabled")
+	// }
 }
 
 func listener() {
+	var (
+		kafkaUrl string
+		topic    string
+		err      error
+	)
 	if !clowder.IsClowderEnabled() {
 		log.Println("clowder disabled")
-		return
-	}
-	kafkaUrl, urlError := getUrl()
-	if urlError != nil {
-		return
-	}
-	topic, topicError := getTopic()
-	if topicError != nil {
-		return
+		kafkaUrl = "localhost:9092"
+		topic = "repos.created"
+	} else {
+		if kafkaUrl, err = getUrl(); err != nil {
+			return
+		}
+		if topic, err = getTopic(); err != nil {
+			return
+		}
 	}
 	partition := 0
 	conn, err := kafka.DialLeader(context.Background(), "tcp", kafkaUrl, topic, partition)
@@ -112,6 +187,11 @@ func listener() {
 }
 
 func apiServer(pingOnly bool) {
+	var (
+		err         error
+		kafkaWriter *kafka.Writer
+		kafkaReader *kafka.Reader
+	)
 	myWidgets := make(map[int64]Widget)
 
 	r := gin.Default()
@@ -121,35 +201,57 @@ func apiServer(pingOnly bool) {
 		})
 	})
 	if !pingOnly {
-		kafkaWriter, error := getKafkaWriter()
-		if error != nil {
-			log.Println("Could not initizlize kafka writer")
-			log.Println(error)
+		if kafkaWriter, err = getKafkaWriter(); err != nil {
+			log.Println("Could not initialize kafka writer")
+			panic(err)
 		}
-		kafkaWriter.BatchTimeout, error = time.ParseDuration("100ms")
-		if error != nil {
-			log.Println(error)
+		if kafkaWriter.BatchTimeout, err = time.ParseDuration("100ms"); err != nil {
+			panic(err)
+		}
+
+		if kafkaReader, err = getKafkaReader(); err != nil {
+			panic(err)
 		}
 
 		r.GET("/kafka/", func(c *gin.Context) {
-			list := make([]Widget, 0, len(myWidgets))
-			for _, w := range myWidgets {
-				list = append(list, w)
+			var (
+				msg    kafka.Message
+				err    error
+				widget Widget
+			)
+			if msg, err = kafkaReader.ReadMessage(c); err != nil {
+				c.AbortWithError(http.StatusNoContent, err)
+				return
 			}
-			c.JSON(http.StatusOK, list)
+			if err = json.Unmarshal(msg.Key, &widget.Key); err != nil {
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			if err = json.Unmarshal(msg.Value, &widget.Payload); err != nil {
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			c.JSON(http.StatusOK, widget)
 		})
 		r.POST("/kafka/", func(c *gin.Context) {
 			var widget Widget
-			if c.BindJSON(&widget) == nil {
-				if widget.Name == "send" {
-					sendMessage(c, kafkaWriter, widget.Id, widget.Name)
-				}
-				log.Println(widget.Id)
-				log.Println(widget.Name)
-				myWidgets[widget.Id] = widget
-
-				c.JSON(http.StatusOK, widget)
+			if err := c.BindJSON(&widget.Payload); err != nil {
+				c.JSON(http.StatusBadRequest, err.Error())
+				return
 			}
+			if widget.Key.Id == 0 {
+				widget.Key.Id = int64(rand.Intn(10000))
+			}
+			if widget.Payload.Comment == "" {
+				widget.Payload.Comment = fmt.Sprintf("message id = %d", widget.Key.Id)
+			}
+			sendMessage(c, kafkaWriter, &widget)
+			log.Printf("Id = %d", widget.Key.Id)
+			log.Printf("Payload: { url: '%s', count: %d, status: '%s' }", widget.Payload.RepositoryURL, widget.Payload.RepositoryCount, widget.Payload.Status)
+			myWidgets[widget.Key.Id] = widget
+			c.Header("Location", fmt.Sprintf("/kafka/%d", widget.Key.Id))
+
+			c.JSON(http.StatusAccepted, widget)
 		})
 		r.GET("/kafka/:id", func(c *gin.Context) {
 			id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
